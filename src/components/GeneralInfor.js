6dbcda4ea643418aa7fc73ba06a414d41a20c07b
clinton wambugu <clintonwambugu@gmail.com>
2021-02-20 12:40:19 +0300

    Create general infor Component

--- src/components/GeneralInfor.js
+++ src/components/GeneralInfor.js
@@ -1,15 +1,17 @@
 import React from 'react';
-// import "./GeneralInfor.css";
+// import "../GeneralInfor.css";
 
 class GeneralExperience extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      name: "john",
+      name: "",
       email: "",
       phone: "",
+      edit: true
     };
     this.handleInput = this.handleInput.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleInput(e) {
@@ -17,34 +19,56 @@ class GeneralExperience extends React.Component {
     this.setState({ [ name ]: value });
   }
 
+  handleSubmit (e) {
+  	//e.preventDefault;
+  	this.setState( prevState => {
+  		return {
+  			edit: !prevState.edit
+  		}
+  	});
+
+  }
+
   render() {
-    console.log(this.state.name);
+  	const formEle = <form >
+  		<label>Name</label><br />
+  		<input 
+  			type="text"
+  			value={this.state.value}
+  			name= "name"
+  			onChange= {this.handleInput}
+  		/><br />
+  		<label>email</label><br />
+  		<input 
+  			type="email"
+  			value={this.state.value}
+  			name= "email"
+  			onChange= {this.handleInput}
+  		/><br />
+  		<label>Phone</label><br />
+  		<input 
+  			type="phone"
+  			value={this.state.value}
+  			name= "phone"
+  			onChange= {this.handleInput}
+  		/><br />
+  	</form>;
+  	const displayResults = <div>
+  		<p> name: {this.state.name}</p>
+  		<p> email: {this.state.email}</p>
+  		<p> phone: {this.state.phone}</p>
+  	</div>;
+  	const btnEle = <button onClick={ this.handleSubmit }>{ this.state.edit? "Subimt" : "Edit" }</button>;
+  	
+  	const res = this.state.edit? formEle : displayResults;
+
     return(
-      <div>
-        <label><strong>General Information</strong></label><br />
-        <label>Name: </label><br/>
-          <input
-            type="text"
-            placeholder="Name"
-            name="name"
-            
-            onChange={this.handleInput}
-          /><br />
-        <label>Email: </label><br/>
-          <input
-            type="email"
-            placeholder="email"
-            name="email"
-            onChange={this.handleInput}
-          /><br />
-          <label>Phone no: </label><br/>
-          <input
-            type="tel"
-            placeholder="phone number"
-            name="phone"
-            onChange={this.handleInput}
-          /><br /><br />
+    	
+      <div>	
+        { res }
+        { btnEle }
       </div>
+      
     )
   }
 }
